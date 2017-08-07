92313a72744c983ec1bf59bde0ea2e4445a267c4
Onil Lekshman <onillekshmanlechu@gmail.com>
2017-08-07 16:27:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,10 @@ app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 
 
 
