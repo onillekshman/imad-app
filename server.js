b3388fe93f01630fe2688d5d79e8ce74f2aaf19b
Onil Lekshman <onillekshmanlechu@gmail.com>
2017-08-08 12:26:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/counter', function (req,res){
     counter = counter  + 1;
     res.send(counter.toString());
     
-})
+});
 
 var names = [];
 app.get('/submit-name', function(req,res){
