9795344b8f34ae7bcdcfc2db520be3ea3528ea39
Onil Lekshman <onillekshmanlechu@gmail.com>
2017-08-08 13:03:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,141 +1,51 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+var button = document.getElementById('counter');
 
-var app = express();
-app.use(morgan('combined'));
-
-var articles = {
-    'article-one':{
-        title:'Article One',
-        heading :'article-One',
-        date : 'Aug 5 2017',
-        content : `<p>
-                This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.
-            </p>
-            <p>
-                This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.
-            </p>
-            <p>
-                This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.
-            </p>`
-        
-    },
-    'article-two':{
-        title:'Article two',
-        heading :'article-two',
-        date : 'Aug 10 2017',
-        content : `<p>
-                This is the article-two content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.
-            </p>
-            <p>
-                This is the article-two content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.
-            </p>`
-        
-        
-    },
-    'article-three':{
-        title:'Article three',
-        heading :'article-three',
-        date : 'Aug 20 2017',
-        content : `<p>
-                This is the article-three content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.This is the article-one content.
-            </p>`
-        
-        
-    }
+button.onclick = function() {
+    var request = new XMLHttpRequest();
     
-    
-    
-};
-function createTemplate(data){
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    var htmlTemplate=`<html>
-    <head>
-        <title>${title}</title>
-        <meta name="viewport" content="width=device-width" initial="scale-1"/>
-         <link href="/ui/style.css" rel="stylesheet" />
+    request.onreadystatechange = function(){
         
-    </head>
-    <body>
-        <div class="container">
-        <div>
-        <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-            ${content}
-        </div>
-        <input type="textarea" id="textarea" placeholder="type here" size="50"></input>
-        <input type="submit" value="submit" id="sumit"></input>
-        <div id="textdisp">
-        </div>
-        </div>
-    </body>
-</html>
-`;
-return htmlTemplate;
-}
-
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-var counter = 0;
-app.get('/counter', function (req,res){
-    counter = counter  + 1;
-    res.send(counter.toString());
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.status === 200){
+                
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    };
+    request.open( 'GET' ,'http://oniljs93.imad.hasura-app.io/counter',true);
+    request.send(null);
     
-});
-
-var names = [];
-app.get('/submit-name', function(req,res){
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
-
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
-});
-
-var texts = ``;
-app.get('/submit-text', function(req,res){
-    var text = req.query.text;
-    texts.push(text);
-    res.send(JSON.stringify(texts));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+    };
+    
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function() {
+        var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.status === 200){
+                
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i<names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+        
+        };
+        var nameInput = document.getElementById('name');
+        var name = nameInput.value;
+        request.open( 'GET' ,'http://oniljs93.imad.hasura-app.io/submit-name?name='+ name, true);
+        request.send(null);
+        
+    };
