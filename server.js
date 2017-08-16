2f721361279eebf240fc3a70b01ee58418793526
Onil Lekshman <onillekshmanlechu@gmail.com>
2017-08-16 13:03:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
+var crypto = require('crypto');
 var config = {
     user: 'oniljs93',
     database: 'oniljs93',
@@ -98,6 +98,16 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt){
+    var hashed = crypto.pbkdf25Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req,res){
+    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
 });
 
 var pool = new Pool(config);
